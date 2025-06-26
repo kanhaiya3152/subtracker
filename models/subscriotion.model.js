8835a01f63a27f143c5035aa641c7636ee374265
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-06-26 12:50:24 +0530

    Refactor subscription model to include additional fields and auto-update logic for renewal date and status

--- models/subscriotion.model.js
+++ models/subscriotion.model.js
@@ -18,5 +18,69 @@ const subscriptionSchema = new mongoose.Schema({
     frequency:{
         type:String,
         enum:['daily','weekly','monthly','yearly'],
+    },
+    category:{
+        type:String,
+        required:true,
+        enum:['sports','news','entertainment','technology','finance','politics']
+    },
+    paymentMethod:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    status:{
+        type:String,
+        enum:['active','cancelled','exptres'],
+        default:'active',
+    },
+    startDate:{
+        type:Date,
+        required:true,
+        validate:{
+            validator:(value)=> value <= new Date(), 
+            message:'Start date must be in the past',
+        }
+    },
+    renewalDate:{
+        type:Date,
+        validate:{
+            validator:(value)=> {
+                return value > this.startDate
+            } , 
+            message:'Renewal date after the start date',
+        }
+    },
+    User:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required:true,
+        index:true
     }
+},{timestamps:true})
+
+// Auto calculate renewal date if missing
+subscriptionSchema.pre('save',function(next){
+    if(!this.renewalDate){
+        const renewalPeriod = {
+            daily:1,
+            weekly:7,
+            monthly:30,
+            yearly:365,
+        }
+        this.renewalDate = new Date(this.startDate)
+        this.renewalDate.setDate(this.renewalDate.getDate() + renewalPeriod[this.frequency])
+    }
+
+    // Auto-update the status if renewal date has passed
+
+    if(this.renewalDate < new Date()){
+        this.status = 'expired'
+    }
+
+    next()
 })
+
+const Subscription = mongoose.model('Subscription',subscriptionSchema)
+
+export default Subscription
