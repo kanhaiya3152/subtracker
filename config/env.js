9b6d34d77165c485631b990e93fa8619bf7b07fa
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-06-28 12:48:44 +0530

    Update env.js to include SERVER_URL and QSTASH variables in environment exports

--- config/env.js
+++ config/env.js
@@ -3,8 +3,10 @@ import { config } from "dotenv";
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const {
-    PORT, NODE_ENV,
+    PORT, NODE_ENV,SERVER_URL,
     DB_URI,
     JWT_SECRET,JWT_EXPIRES_IN,
     ARCJET_KEY,ARCJET_ENV,
+    QSTASH_URL,QSTASH_TOKEN,QSTASH_CURRENT_SIGNING_KEY,QSTASH_NEXT_SIGNING_KEY,
+    EMAIL_PASSWORD,
 } = process.env
